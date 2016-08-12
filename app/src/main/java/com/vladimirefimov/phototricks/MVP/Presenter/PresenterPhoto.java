936720f6b6d0fdb2eeaac96d17d9f25155bf4086
Vladimir Efimov <lesthule@gmail.com>
2016-08-12 16:01:43 +0300

    basic functionality

---
+++ app/src/main/java/com/vladimirefimov/phototricks/MVP/Presenter/PresenterPhoto.java
@@ -0,0 +1,11 @@
+package com.vladimirefimov.phototricks.MVP.Presenter;
+
+/**
+ * Created by vladimir.efimov on 12.08.2016.
+ */
+public class PresenterPhoto {
+
+    public static void takephoto() {
+
+    }
+}
--- app/src/main/java/com/vladimirefimov/phototricks/MainActivity.java
+++ app/src/main/java/com/vladimirefimov/phototricks/MainActivity.java
@@ -1,22 +1,96 @@
 package com.vladimirefimov.phototricks;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+import com.vladimirefimov.phototricks.MVP.Presenter.PresenterPhoto;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.take_photo)
+    Button takePhotoButton;
+    @Bind(R.id.gallery)
+    Button galleryButton;
+    @Bind(R.id.iv_0)
+    ImageView view;
+
+    String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-
-
-
-
-
-
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto();
+            }
+        });
 
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case 777:
+
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    view.setImageBitmap(imageBitmap);
+
+                    Toast.makeText(this, data.toString(), Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+
+    public void takePhoto() {
+
+        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        startActivityForResult(intent, 777);
+
+
+    }
+
 }
