57d64a1b46fa3e5477bcb77f53cc839bd0304dcc
ForestThule <lesthule@gmail.com>
2016-08-15 23:57:21 +0300

    Some shit

--- app/src/main/java/com/vladimirefimov/phototricks/MainActivity.java
+++ app/src/main/java/com/vladimirefimov/phototricks/MainActivity.java
@@ -1,53 +1,64 @@
 package com.vladimirefimov.phototricks;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.FileProvider;
 import android.os.Bundle;
+import android.support.v4.graphics.BitmapCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
-import com.vladimirefimov.phototricks.MVP.Presenter.PresenterPhoto;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    @Bind(R.id.take_photo)
-    Button takePhotoButton;
-    @Bind(R.id.gallery)
+    Button takeFotoButton;
+
     Button galleryButton;
-    @Bind(R.id.iv_0)
-    ImageView view;
 
-    String mCurrentPhotoPath;
+    ImageView iv;
+
+    ArrayList<Uri> currentImagesLocations = new ArrayList<>();
+
+    int counter = currentImagesLocations.size();
+
+//    MyListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        iv = (ImageView) findViewById(R.id.iv_0);
+        takeFotoButton = (Button) findViewById(R.id.take_photo);
+    }
 
-        takePhotoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                takePhoto();
-            }
-        });
+    public void takePhoto() {
+        if (counter < 5) {
+            createPhoto();
+//            dispatchTakePictureIntent();
+        } else Toast.makeText(this, "Фотопленка закончилась", Toast.LENGTH_SHORT).show();
+    }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
@@ -55,42 +66,115 @@ public class MainActivity extends AppCompatActivity {
         if (resultCode == RESULT_OK) {
             switch (requestCode) {
                 case 777:
-
-                    Bundle extras = data.getExtras();
-                    Bitmap imageBitmap = (Bitmap) extras.get("data");
-                    view.setImageBitmap(imageBitmap);
-
-                    Toast.makeText(this, data.toString(), Toast.LENGTH_LONG).show();
+                    showPhoto();
                     break;
             }
         }
     }
 
+    @Override
+    public void onClick(View v) {
+        // do whatever you want here based on the view being passed
+        switch (v.getId()) {
+            case R.id.take_photo:
+                takePhoto();
+                break;
+            //handle multiple view click events
+
+        }
+    }
+
+    private void showPhoto() {
+
+        Bitmap bitmap = BitmapFactory.decodeFile(String.valueOf(currentImagesLocations.get(0)));
+
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>THAT FOTO PATH"+currentImagesLocations.get(0));
+
+//        Picasso.with(MainActivity.this)
+//                .load(currentImagesLocations.get(0))
+//                .into(iv);
+
+        iv.setImageBitmap(bitmap);
+
+
+    }
+
+
+
+
+
+
+    /**
+     * create File for photo plasement
+     * we get filePath here
+     *
+     */
+    private void createPhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = null;
+        try {
+            file = createImageFile();
+            System.out.println(">>>>>>>>>>>>>>>>>>>"+"FileCrtd"+file.getAbsolutePath());
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, file.getAbsolutePath());
+            startActivityForResult(intent, 777);
+            currentImagesLocations.add(Uri.parse(file.getAbsolutePath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private File createImageFile() throws IOException {
         // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        String timeStamp =
+                new SimpleDateFormat("yyMMdd_HHmmss").format(new Date());
+        String imageFileName = "PHOTOTRX_" + timeStamp;
         File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
+                imageFileName,
+                ".JPG",
+                getAlbumDir()
         );
 
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
         return image;
     }
 
+    private File getAlbumDir() {
+        File dir = null;
+        String APP_PATH_SD_CARD = "/Phototricks";
+        String fullPath = Environment.getExternalStorageDirectory().getAbsolutePath() + APP_PATH_SD_CARD;
+        try {
+            dir = new File(fullPath);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+        } catch (Exception e) {
+            Log.w("creating file error", e.toString());
+        }
+        return dir;
+    }
 
-    public void takePhoto() {
-
-        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        startActivityForResult(intent, 777);
-
-
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                System.out.println("File not created");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 777);
+            }
+        }
     }
 
 }
+
+
+
